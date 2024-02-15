99a7e41bee3915cd0720bc631c006a8b240e72ea
Jakub0821 <jakubmigda120@gmail.com>
2024-02-15 14:46:56 +0100

    change SharedLyout.styled, Trening, Trening.styled, MovieDetails and MovieDetails.styled

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,12 +1,36 @@
-import styled from 'styled-components'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { fetchTrendingMovies } from '../../services/api'
+import { TrendingListItem } from './Trending.styled'
 
-export const TrendingListItem = styled.ul`
-    list-style-type: none;
-    margin: 15px 0;
-    text-decoration: none;
-    color: black;
+export const Trending = () => {
+    const location = useLocation()
 
-    &:hover {
-        color: orange;
-    }
-`
+    const [trending, setTrending] = useState([])
+
+    useEffect(() => {
+        const asyncFunction = async () => {
+            try {
+                setTrending(await fetchTrendingMovies())
+            } catch (error) {
+                console.log('Error: ', error)
+            }
+        }
+        asyncFunction()
+    }, [])
+
+    return (
+        <>
+            <h1>Trending today</h1>
+            <ul>
+                {trending.map((tr) => (
+                    <li key={tr.id}>
+                        <Link to={`movies/${tr.id}`} state={{ from: location }}>
+                            <TrendingListItem>{tr.title}</TrendingListItem>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
